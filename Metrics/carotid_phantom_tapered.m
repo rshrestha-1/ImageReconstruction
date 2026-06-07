6a449f18977fac1b597467e165e9abe92fd8de2b
Rebecca Shrestha <rs3123@ic.ac.uk>
2026-06-07 22:31:58 +0100

    Update carotid_phantom_tapered.m

--- Metrics/carotid_phantom_tapered.m
+++ Metrics/carotid_phantom_tapered.m
@@ -32,9 +32,8 @@ labels(mask_wall) = "wall";
 z_idx = linspace(0,1,Nz);
 z_map = reshape(z_idx,1,1,Nz);
 
-% --------------------------------
 % 1. Circular lumen shrinkage
-% --------------------------------
+
 r_lumen_shrink = r_lumen * (1 - 0.5*z_map);  
 % up to 50% narrowing
 
@@ -48,9 +47,7 @@ mask_wall = (r > r_lumen_shrink) & (r <= r_wall);
 v(mask_wall) = Z_wall;
 labels(mask_wall) = "wall";
 
-% --------------------------------
 % 2. Original plaque positions
-% --------------------------------
 lipid_center = [0.2, 0];
 fibrous_center = [0.15, 0];
 calc_center = [0.15, 0];
@@ -59,9 +56,7 @@ r_lipid_xy   = sqrt((x - lipid_center(1)).^2 + (y - lipid_center(2)).^2);
 r_fibrous_xy = sqrt((x - fibrous_center(1)).^2 + y.^2);
 r_calc_xy    = sqrt((x - calc_center(1)).^2 + y.^2);
 
-% --------------------------------
 % 3. Plaque growth (asymmetric)
-% --------------------------------
 fibrous_radius = 0.25 * z_map;
 lipid_radius   = 0.15 * max(z_map - 0.3, 0);
 calc_radius    = 0.08 * max(z_map - 0.6, 0);
